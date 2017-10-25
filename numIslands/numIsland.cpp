0a5571d828db7057e3ec339ad2323629d8f77bdb
Vincent Leung <vincent.leung60@gmail.com>
2017-10-24 17:32:25 -0700

    Accepted number of islands solution

---
+++ numIslands/numIsland.cpp
@@ -0,0 +1,38 @@
+/* 
+ *   https://leetcode.com/problems/number-of-islands/description/
+*/
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        for ( int i = 0; i < grid.size(); i++){
+            for ( int j = 0; j < grid[i].size(); j++){
+                if (grid[i][j] == '1') {
+                    count++;
+                    recursiveDeleteIsland(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    
+    void recursiveDeleteIsland (vector<vector<char>>& grid, int x, int y) {
+        // Delete myself 
+        grid[x][y] = '0';
+        if (grid.size()-1 > x && grid[x+1][y] == '1') {
+            recursiveDeleteIsland(grid, x+1,y);
+        } 
+        if (x > 0 && grid[x-1][y] == '1') {
+            recursiveDeleteIsland(grid, x-1,y);
+        } 
+        if (grid[x].size()-1 > y && grid[x][y+1] == '1') {
+            recursiveDeleteIsland(grid, x, y+1);
+        }
+        if (y > 0 && grid[x][y-1] == '1') {
+            recursiveDeleteIsland(grid, x, y-1);
+        }
+        
+        return;
+    }
+};
